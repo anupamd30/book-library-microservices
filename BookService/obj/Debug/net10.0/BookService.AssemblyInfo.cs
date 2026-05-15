a2bbb7092c2e83c0037a8218fd5e6f4e02e9074c
anupam das <anupamd30@users.noreply.github.com>
2026-05-15 17:33:40 +0530

    Added separate Azure workflows

--- BookService/obj/Debug/net10.0/BookService.AssemblyInfo.cs
+++ BookService/obj/Debug/net10.0/BookService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a468de973c35a20616e7ef74a6d24b94dee9b766")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d9af4e64e1fb8601fa38adc873fec59b933461")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
