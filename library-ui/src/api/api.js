03f4e94269459d1f59e43cf1ee79df2c342d94df
anupam das <anupamd30@users.noreply.github.com>
2026-05-15 19:11:38 +0530

    updated fronted API URL

--- library-ui/src/api/api.js
+++ library-ui/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000" // API Gateway
+  baseURL: "https://api-gateways-api-dzh0dfebgwgkgqgq.centralindia-01.azurewebsites.net" // API Gateway
 });
 
 export default api;
