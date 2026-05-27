dcec7ae1f625ce20fbc2cf49b1b2188fd8b0551d
anupam das <anupamd30@users.noreply.github.com>
2026-05-27 12:17:18 +0530

    Fixed AuthService SQL connectivity and migrations

--- AuthService/obj/Debug/net9.0/AuthService.AssemblyInfo.cs
+++ AuthService/obj/Debug/net9.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc2f3ba578da506a92022d927c9601641ffba9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b20f180c06da53c235379991a685ed3da73279")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
